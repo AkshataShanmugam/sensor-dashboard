a6514eee0ada5fde3014ec53538da5bd3d49d09d
AkshataShanmugam <Akshata.Shanmugam@gmail.com>
2024-11-23 13:11:07 +0530

    Update .gitignore to exclude src/firebase.jsx

--- src/firebase.jsx
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
-const firebaseConfig = {
-  // Add your own configuration key here
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
